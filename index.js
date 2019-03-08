c469d2238d42c7a901af6abd2c6205bf01c7acf5
Marcel <maha93@gmx.de>
2019-03-08 13:01:49 +0100

    removed comments

--- index.js
+++ index.js
@@ -53,19 +53,11 @@ const logger = winston.createLogger({
   format: winston.format.json(),
   defaultMeta: { service: 'user-service' },
   transports: [
-    //
-    // - Write to all logs with level `info` and below to `combined.log`
-    // - Write all logs error (and below) to `error.log`.
-    //
     new winston.transports.File({ filename: 'error.log', level: 'error' }),
     new winston.transports.File({ filename: 'combined.log' })
   ]
 });
 
-//
-// If we're not in production then log to the `console` with the format:
-// `${info.level}: ${info.message} JSON.stringify({ ...rest }) `
-//
 if (process.env.NODE_ENV !== 'production') {
   logger.add(new winston.transports.Console({
     format: winston.format.simple()
@@ -86,9 +78,6 @@ client.on('ready', async () => {
   // DB general stats
   db.execute(config, database => database.query(`SELECT * FROM jabstats`)
   .then(rows => {
-    // console.log(rows);
-    // 343771301405786113
-    // logger.info(client.guilds.get('343771301405786113').channels[0]);
 
     let query;
     if (rows.length < 1) {
