84f5ea87ec1d52800c7315439bdec41bfa97767f
Marcel Hauser <maha93@gmx.de>
2019-05-20 15:14:13 +0200

    improved `>contest list` command

--- cmds/contest.js
+++ cmds/contest.js
@@ -3,13 +3,19 @@ const Discord = require('discord.js');
 
 module.exports.run = async(client, message, args, db) => {
 
+  const cmd = args[0];
+
   let startdate, enddate, contestName, contestDescription, contestType, contestVisibility, typeImage;
   let themes = [];
 
-  if (args[0]) {
-    db.execute(config, database => database.query(`SELECT * FROM contest WHERE id = '${args[0]}'`)
+  let currentDate = new Date();
+
+  if (!cmd) {
+    message.channel.send('send current contest theme');
+  } else if (/^\d+$/.test(cmd)) {
+    db.execute(config, database => database.query(`SELECT * FROM contest WHERE id = '${cmd}'`)
     .then(rows => {
-      if (rows.length < 1) return message.channel.send(`Couldn't find the contest`);
+      if (rows.length < 1) return message.channel.send(`Couldn't find a contest with id: ${cmd}`);
 
       startdate = new Date(rows[0].startdate);
       startdateStr = ('0' + (startdate.getMonth() + 1)).slice(-2) + '/' + ('0' + startdate.getDate()).slice(-2) + '/' + startdate.getFullYear() + ' ' + ('0' + startdate.getHours()).slice(-2) + ':' + ('0' + startdate.getMinutes()).slice(-2);
@@ -21,7 +27,7 @@ module.exports.run = async(client, message, args, db) => {
       contestType = rows[0].type;
       contestVisibility = rows[0].visibility;
 
-      db.execute(config, database => database.query(`SELECT * FROM contestThemes WHERE contestId = '${args[0]}' ORDER BY startdate`)
+      db.execute(config, database => database.query(`SELECT * FROM contestThemes WHERE contestId = '${cmd}' ORDER BY startdate`)
       .then(rows => {
 
         if (rows.length < 1) {
@@ -31,7 +37,7 @@ module.exports.run = async(client, message, args, db) => {
           for (let i = 0; i < rows.length; i++) {
             let themeStart = new Date(rows[i].startdate);
             // if the startdate of the theme is in the future && its a hidden contest hide the theme
-            if (themeStart > startdate && contestVisibility == 'hidden') {
+            if (themeStart > currentDate && contestVisibility == 'hidden') {
               themes.push('- ' + '\\*'.repeat(rows[i].name.length));
             } else {
               themes.push(`- ${rows[i].name}`);
@@ -43,13 +49,13 @@ module.exports.run = async(client, message, args, db) => {
         let embed = new Discord.RichEmbed()
         .setAuthor('Contest!', (contestVisibility == 'hidden') ? 'https://ice-creme.de/images/jabstat/hidden-icon.jpg' : 'https://ice-creme.de/images/jabstat/public-icon.jpg')
         .setTitle(contestName)
-        .setDescription(`${contestDescription}\n\nThis is a ${contestType} contest`)
+        .setDescription(`${contestDescription}\n\n*This is a ${contestType} contest*`)
         .setColor((contestVisibility == 'hidden') ? '#e74c3c' : '#3498db')
         .addBlankField()
         .addField('Start', startdateStr, true)
         .addField('Deadline', enddateStr, true)
         .addField('Themes:', `${themes.join('\n')}`)
-        .setFooter(`beep boop • contest ID: ${args[0]}`, client.user.avatarURL);
+        .setFooter(`beep boop • contest ID: ${cmd}`, client.user.avatarURL);
         message.channel.send({embed: embed});
 
         return;
@@ -61,19 +67,31 @@ module.exports.run = async(client, message, args, db) => {
     .catch(err => {
       throw err;
     });
-  } else {
+  } else if (cmd == 'list') {
     let contests = [];
-    db.execute(config, database => database.query(`SELECT * FROM contest`)
+    db.execute(config, database => database.query(`SELECT * FROM contest ORDER BY startdate`)
     .then(rows => {
       if (rows.length < 1) return message.channel.send(`There are no contests :sad_cett:`);
+
       for (let i = 0; i < rows.length; i++) {
-        contests.push(`[${rows[i].id}] \t${rows[i].name}`);
+        let contestStartDate = new Date(rows[i].startdate);
+        let contestEndDate = new Date(rows[i].enddate);
+
+        // if the contest enddate is in the past -> line through
+        if (contestEndDate < currentDate) {
+          contests.push(`\`${rows[i].id}\` | \t~~${rows[i].name}~~`);
+        } else {
+          contests.push(`\`${rows[i].id}\` | \t${rows[i].name}`);
+        }
       }
 
       let embed = new Discord.RichEmbed()
-      .setTitle('Contest list:')
-      .setDescription(`${contests.join('\n')}`)
-      .setColor('AQUA')
+      .setAuthor('Contest list')
+      .setDescription('type `>contest ID` to get more informations')
+      .addBlankField()
+      // add Field "there are currently X contests" OR "there are no contests at the moment"
+      .addField('All contests:', `${contests.join('\n')}`)
+      .setColor('#3498db')
       .setFooter(`beep boop`, client.user.avatarURL);
       message.channel.send({embed: embed});
 
@@ -82,6 +100,10 @@ module.exports.run = async(client, message, args, db) => {
     .catch(err => {
       throw err;
     });
+  } else if (cmd == 'help') {
+    message.channel.send('help command');
+  } else {
+    message.channel.send('type `>contest help` for more informations');
   }
 
 }
