b84ffc9ad9403f01b8c4a15cff2b44a320472c3b
Marcel Hauser <maha93@gmx.de>
2019-06-07 09:47:58 +0200

    changed the activity to the help command :angery_tears: screw u kev

--- events/ready.js
+++ events/ready.js
@@ -6,26 +6,27 @@ module.exports = (client) => {
   console.log(logColor.blackBlue, `${client.user.username} is ready to steal your data >:)`);
   logger.info('\x1b[92mBot started\x1b[0m', {logType: 'start', time: Date.now()});
 
-  let activity = [
-    'collecting data',
-    'stealing your data',
-    'extracting stats',
-    'collecting stats',
-    'give me your data',
-    'stealing your informations',
-    'borrowing user data',
-    'stealing user data',
-    'pretend to be google',
-    'stealing userdata 101',
-    'statistics for dummies',
-    'how to steal data',
-    'charts'
-  ];
+  // let activity = [
+  //   'collecting data',
+  //   'stealing your data',
+  //   'extracting stats',
+  //   'collecting stats',
+  //   'give me your data',
+  //   'stealing your informations',
+  //   'borrowing user data',
+  //   'stealing user data',
+  //   'pretend to be google',
+  //   'stealing userdata 101',
+  //   'statistics for dummies',
+  //   'how to steal data',
+  //   'charts'
+  // ];
+  let activity = ['>c help'];
 
-  client.user.setActivity(`${activity.random()}`, {type: 'WATCHING'});
+  client.user.setActivity(`${activity.random()}`, {type: 'PLAYING'});
 
   setInterval(() => {
-    client.user.setActivity(`${activity.random()}`, {type: 'WATCHING'});
+    client.user.setActivity(`${activity.random()}`, {type: 'PLAYING'});
   }, 1000 * 60 * 3);
 }
 
